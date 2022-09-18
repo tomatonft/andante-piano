0483ddb60fa51e7d04a5929d3b851bd6204a369e
fantasia <87534145+tomatonft@users.noreply.github.com>
2022-09-18 23:26:57 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -51,4 +51,19 @@ function hamburger() {
   }
 }
 
+/*=================================================
+  スムーススクロール
+  ===================================================*/
+  
+  $('a[href^="#"]').click(function(){
+    let href= $(this).attr("href");
+
+    let target = $(href == "#" || href == "" ? 'html' : href);
+
+    let position = target.offset().top;
+    
+    $("html, body").animate({scrollTop:position}, 600, "swing");
+    return false;
+  });
+
 });
